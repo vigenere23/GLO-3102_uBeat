de162bc0db04be7f6d2fea23f7afdb62bb9165a1
Maxime St-Pierre <maxime.st-pierre.5@ulaval.ca>
2019-01-23 18:09:27 -0500

    added new techno for the project

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
-  <div id="app">
-    <nav-menu></nav-menu>
-    <router-view></router-view>
-  </div>
+  <v-app id="inspire">
+    <div id="app">
+      <nav-menu></nav-menu>
+      <router-view></router-view>
+    </div>
+  </v-app>
 </template>
 
 <script>
-import Navigation from '@/components/Navigation';
+  import Navigation from '@/components/Navigation';
 
-export default {
-  name: 'app',
-  components: {
-    'nav-menu': Navigation
-  }
-};
+  export default {
+    name: 'app',
+    components: {
+      'nav-menu': Navigation
+    }
+  };
 </script>
 
 <style>
