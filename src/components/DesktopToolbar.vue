5fa5753b04d0073cb595ea6f91411f5741d47963
Maxime St-Pierre <maxime.st-pierre.5@ulaval.ca>
2019-01-25 23:00:17 -0500

    added support for mobile, tablet is still missing

---
+++ src/components/DesktopToolbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-toolbar id="desktopToolbar">
+    <v-btn flat icon>
+      <router-link to="/artist"><v-icon>headset</v-icon></router-link>
+    </v-btn>
+    <v-toolbar-title class="toolbarTitle"><router-link to="/">UBeat</router-link></v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-text-field
+      id="desktopSearchField"
+      solo
+      flat
+      box
+      hide-details
+      style="max-width: 600px; min-width: 600px;"
+      placeholder="Search"
+      prepend-inner-icon="search">
+    </v-text-field>
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn flat>Playlists</v-btn>
+      <v-menu offset-y>
+        <v-btn slot="activator" flat>Username
+          <v-avatar id="userAvatar"><img src="/static/generic-avatar.png"></v-avatar>
+        </v-btn>
+        <v-list>
+          <v-list-tile
+            v-for="(item, index) in items"
+            :key="index"
+            @click=""
+          >
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+<script>
+  export default {
+    name: 'desktopToolbar',
+    data: () => ({
+      items: [
+        { title: 'Profile' },
+        { title: 'Settings' },
+        { title: 'Logout' }
+      ]
+    })
+  };
+</script>
+<style>
+  #desktopSearchField {
+    color: black;
+  }
+</style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,48 +1,21 @@
-<link rel = "stylesheet" type = "text/css" href = "../css/style.css" />
+<link rel="stylesheet" type="text/css" href="../css/style.css"/>
 <template>
   <div id="navigationContainer">
-    <div id="mobileToolbarContainer" class="hidden-md-and-up">
-      <v-toolbar floating></v-toolbar>
+    <div id="mobileToolbarContainer" class="hidden-lg-and-up">
+      <mobileToolbar/>
     </div>
-    <div id="desktopToolbarContainer" class="hidden-sm-and-down">
-      <v-toolbar id="desktopToolbar">
-        <v-toolbar-icon><v-icon>headset</v-icon></v-toolbar-icon>
-        <v-toolbar-title>UBeat</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-text-field
-          id="desktopSearchField"
-          solo
-          flat
-          box
-          prepend-inner-icon="search">
-        </v-text-field>
-        <v-spacer></v-spacer>
-        <v-toolbar-items>
-          <v-btn flat>Playlists</v-btn>
-          <v-menu offset-y>
-            <v-btn slot="activator" flat>Username<v-avatar id="userAvatar"><img src="/static/generic-avatar.png"></v-avatar></v-btn>
-            <v-list>
-              <v-list-tile
-                v-for="(item, index) in items"
-                :key="index"
-                @click=""
-              >
-                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-              </v-list-tile>
-            </v-list>
-          </v-menu>
-        </v-toolbar-items>
-      </v-toolbar>
+    <div id="desktopToolbarContainer" class="hidden-md-and-down">
+      <desktopToolbar/>
     </div>
   </div>
 </template>
 
-<style>
-  #desktopSearchField{
-    color: black;
-  }
-</style>
+<script>
+  import DesktopToolbar from './DesktopToolbar';
+  import MobileToolbar from './MobileToolbar';
 
-<script type="text/javascript" src="../js/Navigation.js">
+  export default {
+    components: { MobileToolbar, DesktopToolbar }
+  };
 </script>
 
