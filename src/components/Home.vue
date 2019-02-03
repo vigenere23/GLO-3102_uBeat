9a205dbf60b96b38f3dbd977d25c8abee91da74b
Olivier <olivier.gingras.1@ulaval.ca>
2019-02-03 13:10:35 -0500

    It's a start...

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,40 @@
 <template>
-    <div>
+    <div class="background">
         <h1>Home</h1>
-        <div>GLO-3102 Home</div>
+        <div class="centerVertical">
+            <div class="centerHorizontal bigger">
+              <div> <big> Welcome to the best page in the world </big> </div>
+              <div> Find your artist</div>
+              <div> And listen to his best album !</div>
+              <div> <a href="Navigation.vue"> Click here </a> to start your journey </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <style>
+
+  .bigger{
+    font-size: xx-large;
+  }
+
+  .background{
+    background-color: red;
+  }
+
+  .centerHorizontal{
+      height: 100px;
+    position: absolute;
+    margin:auto;
+    bottom:0;
+    top:0;
+
+  }
+
+  .centerVertical{
+    display: flex;
+    justify-content: center;
+  }
 </style>
 
 <!--homePageTest-->
