e11a21438c36fa9a33d8f9db2c02cfdf8eff7cd5
Maxime St-Pierre <maxime.st-pierre.5@ulaval.ca>
2019-02-04 17:42:22 -0500

    latest push avant la reunion

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -11,7 +11,7 @@
                   @click.stop="drawer = !drawer">
                 </v-toolbar-side-icon>
                 <v-btn flat id="toolbarHomeBtn" v-if="isFixed">
-                  <router-link to="/"><v-icon>headset</v-icon>UBeat</router-link>
+                  <router-link to="/"><v-icon>headset</v-icon>  UBeat</router-link>
                 </v-btn>
                 <v-spacer v-if="isFixed"></v-spacer>
                 <v-text-field
@@ -19,20 +19,49 @@
                   solo
                   flat
                   hide-details
+                  style="max-width: 800px; width: 500px;"
+                  @blur="inputIsSelectedIcon = 'search'"
+                  @input="inputIsSelectedIcon = 'clear'"
+                  :append-icon="inputIsSelectedIcon"
                   placeholder="Search">
                 </v-text-field>
-                <v-btn icon>
-                  <v-icon>clear</v-icon>
-                </v-btn>
-                <v-btn icon>
-                  <v-icon>search</v-icon>
-                </v-btn>
                 <v-spacer v-if="isFixed"></v-spacer>
                 <desktopToolbarItems :is-fixed="isFixed"/>
               </v-toolbar>
             </v-flex>
           </v-layout>
         </v-container>
+      </v-layout>
+      <v-layout>
+        <v-navigation-drawer
+          v-model="drawer"
+          absolute
+          temporary>
+          <v-list class="pa-1">
+            <v-list-tile avatar>
+              <v-list-tile-avatar>
+                <img src="/static/generic-avatar.png">
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title>User</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+          <v-list class="pt-0" dense>
+            <v-divider></v-divider>
+            <v-list-tile
+              v-for="item in navigationDrawerItems"
+              :key="item.title"
+              @click="">
+              <v-list-tile-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-navigation-drawer>
       </v-layout>
     </div>
 </template>
@@ -45,20 +74,26 @@
     components: { DesktopToolbarItems },
     data() {
       return {
+        drawer: null,
         isFixed: true,
-        inputIsSelected: false,
+        isTablet: false,
+        inputIsSelectedIcon: 'search',
+        navigationDrawerItems: [
+          { title: 'Home', icon: 'dashboard', ref: '/' },
+          { title: 'Playlists', icon: 'queue_music', ref: '/playlists' },
+          { title: 'Profile', icon: 'person', ref: '/profile' },
+          { title: 'Settings', icon: 'settings', ref: '/settings' },
+          { title: 'Logout', icon: 'logout', ref: '/logout' }
+        ],
       };
     },
     mounted() {
       this.$nextTick(() => {
         window.addEventListener('resize', () => {
           this.isFixed = window.innerWidth > 1024;
+          this.isTablet = window.innerWidth < 1024 && window.innerWidth > 640;
         });
-        document.getElementById('searchField').onfocus = function isSelected() {
-          this.inputIsSelected = true;
-        };
       });
     },
   };
 </script>
-
