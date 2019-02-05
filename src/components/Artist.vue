a02c79f6b5b378a4b07f97ac6a5bf890b73c5fe9
GASTP33 <gabriel.st-pierre.6@ulaval.ca>
2019-02-05 16:29:47 -0500

    fixed items width + added release year + added lists for playlists, singles, popular songs, etc.

--- src/components/Artist.vue
+++ src/components/Artist.vue
@@ -8,9 +8,44 @@
         <a href="https://itunes.apple.com/ca/artist/tame-impala/290242959" target="_blank">iTunes</a>
       </div>
 
-      <div class="songs">
+      <div class="cover-list">
+        <h3>Popular Songs</h3>
         <ul>
-          <li></li>
+          <li>
+            <a href="javascript:">
+              <img src="https://static.stereogum.com/uploads/2014/08/Tame-Impala-Feels-Like-We-Only-Go-Backwards-compressed.jpeg">
+              <p class="name">Feels Like We Only Go Backwards</p>
+              <p class="year">2012</p>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:">
+              <img src="https://static.stereogum.com/uploads/2015/03/tame-impala_let-it-happen-640x640.jpg">
+              <p class="name">Let It Happen</p>
+              <p class="year">2015</p>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:">
+              <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/554fc131138747.5642c93bdd5da.jpg">
+              <p class="name">Elephant</p>
+              <p class="year">2012</p>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:">
+              <img src="https://t2.genius.com/unsafe/220x220/https%3A%2F%2Fimages.genius.com%2F6b293f5822fd78fb33327d961ba72901.500x500x1.jpg">
+              <p class="name">Why Won't You Make Up Your Mind</p>
+              <p class="year">2010</p>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:">
+              <img src="https://www.music-bazaar.com/album-images/vol9/471/471848/2296617-big/Apocalypse-Dreams-Single-cover.jpg">
+              <p class="name">Apocalypse Dreams</p>
+              <p class="year">2012</p>
+            </a>
+          </li>
         </ul>
       </div>
 
@@ -20,31 +55,97 @@
           <li>
             <a href="javascript:">
               <img src="https://payload.cargocollective.com/1/4/158872/9666476/Tame-Impala-Currents-final-packshot-1200px_800.jpg">
-              <p>Currents</p>
+              <p class="name">Currents</p>
+              <p class="year">2015</p>
             </a>
           </li>
           <li>
             <a href="javascript:">
-              <img src="https://static.stereogum.com/uploads/2014/08/Tame-Impala-Feels-Like-We-Only-Go-Backwards-compressed.jpeg">
-              <p>Feels Like We Only Go Backwards</p>
+              <img src="https://is4-ssl.mzstatic.com/image/thumb/Music/v4/2b/2c/c3/2b2cc397-7b5a-871c-cea5-4630d7253f76/UMG_cvrart_00602537108527_01_RGB72_1200x1200_12UMGIM38392.jpg/320x0w.jpg">
+              <p class="name">Lonerism</p>
+              <p class="year">2012</p>
             </a>
           </li>
           <li>
             <a href="javascript:">
               <img src="http://i.imgur.com/iV6N6xe.jpg">
-              <p>Innerspeaker</p>
+              <p class="name">Innerspeaker</p>
+              <p class="year">2010</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="cover-list">
+        <h3>Playlists</h3>
+        <ul>
+          <li>
+            <a href="javascript:">
+              <img src="https://is4-ssl.mzstatic.com/image/thumb/Features125/v4/fe/66/15/fe661571-ae43-3ffa-5739-f6da4893a4a5/source/320x320sr.jpg">
+              <p class="name">Essentials</p>
+              <p class="year"></p>
             </a>
           </li>
           <li>
             <a href="javascript:">
-              <img src="https://i.pinimg.com/originals/52/fb/7e/52fb7e1accd57afc524a49c8a605f35b.jpg">
-              <p>Ripples</p>
+              <img src="https://is5-ssl.mzstatic.com/image/thumb/Features111/v4/3f/bd/f2/3fbdf2cb-005a-b617-29aa-a169e231e889/source/320x320sr.jpg">
+              <p class="name">Next Steps</p>
+              <p class="year"></p>
             </a>
           </li>
           <li>
             <a href="javascript:">
-              <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/554fc131138747.5642c93bdd5da.jpg">
-              <p>Elephant</p>
+              <img src="https://is2-ssl.mzstatic.com/image/thumb/Features118/v4/54/27/bf/5427bf24-e70f-5ac2-72fe-ea90ed6075f1/source/320x320sr.jpg">
+              <p class="name">Influences</p>
+              <p class="year"></p>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="cover-list">
+        <h3>Singles &amp; EP</h3>
+        <ul>
+          <li>
+            <a href="javascript:">
+              <img src="https://is1-ssl.mzstatic.com/image/thumb/Music114/v4/27/57/8d/27578d4e-8692-483f-c6c4-df948cdce0ab/855230008308.jpg/320x0w.jpg">
+              <p class="name">Whiplash</p>
+              <p class="year">2019</p>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:">
+              <img src="https://is2-ssl.mzstatic.com/image/thumb/Music128/v4/f5/2b/31/f52b316f-7ddf-cb83-64ff-31416feaccc7/855230008261.jpg/320x0w.jpg">
+              <p class="name">Only You</p>
+              <p class="year">2018</p>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:">
+              <img src="https://is3-ssl.mzstatic.com/image/thumb/Music125/v4/a9/bf/ed/a9bfed03-ce92-e16b-6ce9-d9e0fe1fd1ac/810762034119_cover.jpg/320x0w.jpg">
+              <p class="name">My Life</p>
+              <p class="year">2018</p>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:">
+              <img src="https://is3-ssl.mzstatic.com/image/thumb/Music118/v4/24/64/93/2464933e-9e41-fbcd-2714-2d0f7ca78c86/UMG_cvrart_00602567155454_01_RGB72_3000x3000_17UM1IM40840.jpg/320x0w.jpg">
+              <p class="name">Currents B-Sides</p>
+              <p class="year">2017</p>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:">
+              <img src="https://is4-ssl.mzstatic.com/image/thumb/Music118/v4/f1/18/7b/f1187b66-4432-3f54-9004-81128ac5400d/00602577262067.rgb.jpg/320x0w.jpg">
+              <p class="name">Lonerism B-Sides</p>
+              <p class="year">2013</p>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:">
+              <img src="https://is3-ssl.mzstatic.com/image/thumb/Music/61/79/44/mzi.ffecmvha.jpg/320x0w.jpg">
+              <p class="name">Tame Impala - EP</p>
+              <p class="year">2008</p>
             </a>
           </li>
         </ul>
@@ -88,16 +189,6 @@
     margin-top: 16px;
   }
 
-  #artist-page .songs {
-    list-style: none;
-    padding: 0;
-  }
-
-  #artist-page .songs ul {
-    list-style: none;
-    padding: 0;
-  }
-
   .cover-list {
     width: 100%;
     margin-top: 64px;
@@ -112,13 +203,19 @@
 
   .cover-list ul {
     display: flex;
-    overflow: auto;
-    padding: 0;
+    overflow-x: auto;
+    padding: 8px 0;
     list-style: none;
   }
 
+  .cover-list li {
+    flex-grow: 0;
+    flex-shrink: 0;
+    flex-basis: 200px;
+  }
+
   .cover-list li:not(:last-child) {
-    padding-right: 32px;
+    margin-right: 32px;
   }
 
   .cover-list a {
@@ -141,15 +238,23 @@
     outline: none;
   }
 
-  .cover-list p {
+  .cover-list p.name {
+    margin: 0;
     margin-top: 8px;
     text-transform: capitalize;
     font-size: 16px;
     line-height: 1.25em;
   }
 
+  .cover-list p.year {
+    margin: 0;
+    font-size: 14px;
+    color: rgba(255, 255, 255, 0.68);
+  }
+
   .cover-list img {
-    width: 200px;
+    display: block;
+    width: 100%;
   }
 
   @media screen and (max-width: 640px) {
@@ -177,7 +282,7 @@
       font-size: 24px;
     }
 
-    .cover-list img {
+    .cover-list li {
       width: 160px;
     }
   }
@@ -186,7 +291,4 @@
 </style>
 <!--
 TODO: Logo itunes?
-TODO: Liste de singles
-TODO: Playlists
-TODO: Liste de chansons populaires
 -->
