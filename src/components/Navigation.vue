56d58f6ec7f3a69aadcc490056d7f742a7e00fb4
Maxime St-Pierre <maxime.st-pierre.5@ulaval.ca>
2019-02-08 21:37:19 -0500

    new version less CSS of the navigation bar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,15 +1,175 @@
 <link rel="stylesheet" type="text/css" href="../css/style.css"/>
-<template>
+<template id="navigation-template">
   <div id="navigationContainer">
-      <toolbar/>
+    <v-toolbar
+      dark
+      app
+      height="64px"
+      :fixed="toolbar.fixed"
+      :clipped-left="toolbar.clippedLeft">
+      <v-toolbar-side-icon v-if="windowWidth <= 600"
+        @click.stop="drawer.open = !drawer.open">
+      </v-toolbar-side-icon>
+      <v-toolbar-title v-if="windowWidth > 600">
+        <v-btn flat>
+          <v-icon>headset</v-icon>
+          UBeat
+        </v-btn>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-text-field
+        id="searchField"
+        flat
+        solo
+        hide-details
+        style="max-width: 800px; width: 500px;"
+        clearable
+        append-icon='search'
+        placeholder="Search">
+      </v-text-field>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-navigation-drawer
+      app
+      dark
+      :clipped="windowWidth > 600"
+      :fixed="windowWidth > 600"
+      :permanent="windowWidth > 600"
+      :mini-variant.sync="drawer.mini"
+      v-model="drawer.open">
+      <v-toolbar flat class="transparent">
+        <v-list>
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <img src="/static/generic-avatar.png">
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>User</v-list-tile-title>
+            </v-list-tile-content>
+            <v-list-tile-action v-if="windowWidth > 600 && windowWidth < 1264">
+              <v-btn
+                icon
+                @click.stop="drawer.mini = !drawer.mini">
+                <v-icon>chevron_left</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+      <v-list>
+        <v-divider></v-divider>
+        <v-list-tile
+          v-for="item in navigationDrawerItems"
+          :key="item.title"
+          @click="">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+      <v-list dense>
+        <v-divider></v-divider>
+        <v-list-tile
+          v-for="item in userMenuItem"
+          :key="item.title"
+          @click="">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
   </div>
 </template>
 
 <script>
-  import Toolbar from './Toolbar';
-
   export default {
-    components: { Toolbar }
+    tempplate: '#navigation-template',
+    data() {
+      return {
+        screenSize: '',
+        windowWidth: 0,
+        drawer: {
+          open: false,
+          clipped: true,
+          fixed: true,
+          permanent: true,
+          mini: false,
+          debounce: false,
+        },
+        toolbar: {
+          fixed: true,
+          clippedLeft: true,
+        },
+        navigationDrawerItems: [
+          { title: 'Home', icon: 'dashboard', ref: '/' },
+          { title: 'Playlists', icon: 'queue_music', ref: '/playlists' }
+        ],
+        userMenuItem: [
+          { title: 'Profile', icon: 'person', ref: '/profile' },
+          { title: 'Settings', icon: 'settings', ref: '/settings' },
+          { title: 'Logout', icon: 'logout', ref: '/logout' }
+        ],
+        toggleMiniDrawer() {
+
+        },
+        setDesktopMode() {
+          this.drawer.clipped = true;
+          this.drawer.fixed = true;
+          this.drawer.permanent = true;
+          this.drawer.mini = false;
+        },
+        setTabletMode() {
+          this.drawer.clipped = true;
+          this.drawer.fixed = true;
+          this.drawer.permanent = true;
+          this.drawer.mini = true;
+        },
+        setPhoneMode() {
+          this.drawer.open = false;
+          this.drawer.clipped = false;
+          this.drawer.fixed = false;
+          this.drawer.permanent = false;
+          this.drawer.mini = false;
+        },
+        /*toggleMini() {
+          if (this.windowWidth > 600 && this.windowWidth < 1264) {
+            if ()
+          } else {
+            this.drawer.open = !this.drawer.open;
+          }
+        },*/
+      };
+    },
+    mounted() {
+      this.$nextTick(() => {
+        window.onresize = () => {
+          console.log('RESIZE EVENT');
+          if (this.windowWidth > 600 && this.windowWidth < 1264 && this.drawer.debounce === false) {
+            this.drawer.mini = true;
+            this.drawer.debounce = true;
+          } else if (this.windowWidth >= 1264 && this.drawer.debounce === true) {
+            this.drawer.mini = false;
+            this.drawer.debounce = false;
+          } else if (this.windowWidth <= 600) {
+            this.drawer.mini = false;
+            this.drawer.debounce = false;
+          }
+          this.windowWidth = window.innerWidth;
+        };
+      });
+    },
   };
 </script>
 
+<style>
+  html {
+    overflow-y: hidden;
+  }
+</style>
--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,94 +1,63 @@
 <template>
-    <div id="toolbar">
-      <v-layout align-center justify-center>
-        <v-container>
-          <v-layout>
-            <v-flex>
-              <v-toolbar
-              :fixed="toolbar.fixed"
-              :clipped-left="toolbar.clippedLeft">
-                <v-toolbar-side-icon
-                  v-if="!toolbar.fixed"
-                  @click.stop="toggleDrawer">
-                </v-toolbar-side-icon>
-                <v-btn flat id="toolbarHomeBtn" v-if="toolbar.fixed">
-                  <router-link to="/"><v-icon>headset</v-icon>  UBeat</router-link>
-                </v-btn>
-                <v-spacer v-if="toolbar.fixed"></v-spacer>
-                <v-text-field
-                  id="searchField"
-                  flat
-                  solo
-                  hide-details
-                  style="max-width: 800px; width: 500px;"
-                  clearable
-                  append-icon='search'
-                  placeholder="Search">
-                </v-text-field>
-                <v-spacer v-if="toolbar.fixed"></v-spacer>
-                <desktopToolbarItems :fixed="toolbar.fixed"/>
-              </v-toolbar>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-layout>
-      <v-layout>
-        <v-navigation-drawer
-          :clipped="drawer.clipped"
-          :fixed="drawer.fixed"
-          :permanent="drawer.permanent"
-          :mini-variant="drawer.mini"
-          v-model="drawer.open">
-          <v-list class="pa-1">
-            <v-list-tile avatar>
-              <v-list-tile-avatar>
-                <img src="/static/generic-avatar.png">
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-title>User</v-list-tile-title>
-              </v-list-tile-content>
-              <v-list-tile-action>
-                <v-btn
-                  @click.stop="mini = !mini"
-                  icon
-                >
-                  <v-icon>chevron_left</v-icon>
-                </v-btn>
-              </v-list-tile-action>
-            </v-list-tile>
-          </v-list>
-          <v-list class="pt-0" dense>
-            <v-divider></v-divider>
-            <v-list-tile
-              v-for="item in navigationDrawerItems"
-              :key="item.title"
-              @click="">
-              <v-list-tile-action>
-                <v-icon>{{ item.icon }}</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-        </v-navigation-drawer>
-      </v-layout>
-    </div>
+  <v-toolbar
+    app
+    dark
+    :fixed="toolbar.fixed"
+    :clipped-left="toolbar.clippedLeft">
+    <v-toolbar-side-icon
+      v-if="!toolbar.fixed">
+    </v-toolbar-side-icon>
+    <v-toolbar-title>
+      <v-btn flat>
+        <v-icon>headset</v-icon>
+        UBeat
+      </v-btn>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-text-field
+      id="searchField"
+      flat
+      solo
+      hide-details
+      style="max-width: 800px; width: 500px;"
+      clearable
+      append-icon='search'
+      placeholder="Search">
+    </v-text-field>
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn flat>Playlists</v-btn>
+      <v-menu offset-y>
+        <v-btn slot="activator" flat>Username
+          <v-avatar id="userAvatar"><img src="/static/generic-avatar.png"></v-avatar>
+        </v-btn>
+        <v-list>
+          <v-list-tile
+            v-for="(item, index) in toolbarMenuItems"
+            :key="index"
+            @click=""
+          >
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-toolbar-items>
+  </v-toolbar>
 </template>
 
 <script>
-  import DesktopToolbarItems from './DesktopToolbarItems';
 
   export default {
+    components: {},
+    el: '#toolbar',
     name: 'Toolbar',
-    components: { DesktopToolbarItems },
     data() {
       return {
         drawer: {
           open: false,
           clipped: false,
           fixed: false,
-          permanent: false,
+          permanent: true,
           mini: false,
         },
         toolbar: {
@@ -102,64 +71,24 @@
           { title: 'Settings', icon: 'settings', ref: '/settings' },
           { title: 'Logout', icon: 'logout', ref: '/logout' }
         ],
+        toolbarMenuItems: [
+          { title: 'Profile', ref: '/profile' },
+          { title: 'Settings', ref: '/settings' },
+          { title: 'Logout', ref: '/logout' }
+        ],
         methods: {
-          makeDrawerDesktop() {
-            this.drawer.open = false;
-            this.drawer.clipped = false;
-            this.drawer.fixed = true;
-            this.drawer.permanent = false;
-            this.drawer.mini = false;
-            this.toolbar.fixed = true;
-            this.toolbar.clippedLeft = false;
-          },
-          makeDrawerTablet() {
-            this.drawer.open = true;
-            this.drawer.clipped = false;
-            this.drawer.fixed = true;
-            this.drawer.permanent = true;
-            this.drawer.mini = false;
-            this.toolbar.fixed = false;
-            this.toolbar.clippedLeft = false;
-          },
-          makeDrawerPhone() {
-            this.drawer.open = false;
-            this.drawer.clipped = true;
-            this.drawer.fixed = true;
-            this.drawer.permanent = true;
-            this.drawer.mini = false;
-            this.toolbar.fixed = true;
-            this.toolbar.clippedLeft = false;
-          },
           toggleMiniDrawer() {
             this.drawer.mini = !this.drawer.mini;
           },
           toggleDrawer() {
-            if (this.drawer.permanent) {
-              this.drawer.permanent = !this.drawer.permanent;
-              // set the clipped state of the drawer and toolbar
-              this.drawer.clipped = true;
-              this.toolbar.clippedLeft = true;
-            } else {
-              // normal drawer
-              this.drawer.open = !this.drawer.open;
-            }
-          }
+            this.drawer.open = !this.drawer.open;
+          },
         }
       };
     },
     mounted() {
       this.$nextTick(() => {
         window.addEventListener('resize', () => {
-          if (window.innerWidth > 1024) {
-            console.log('DESKTOP MODE ACTIVATED');
-            this.methods.makeDrawerDesktop();
-          } else if (window.innerWidth < 1024 && window.innerWidth > 640) {
-            console.log('TABLET MODE ACTIVATED');
-            this.methods.makeDrawerTablet();
-          } else {
-            console.log('PHONE MODE ACTIVATED');
-            this.methods.makeDrawerPhone();
-          }
         });
       });
     },
