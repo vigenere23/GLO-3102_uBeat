b4d3867a06a013fa19db700408cb500148f4a7c5
GASTP33 <gabriel.st-pierre.6@ulaval.ca>
2019-02-11 19:11:22 -0500

    created the components - no test done yet

---
+++ src/components/Cover.vue
@@ -0,0 +1,89 @@
+<template>
+  <a class="cover" href="javascript:">
+    <img src="{{ imageSrc }}">
+    <p class="name">{{ name }}</p>
+    <p class="year">{{ year }}</p>
+  </a>
+</template>
+
+<script>
+export default {
+  name: "cover",
+  porps: {
+    type: String,
+    name: String,
+    year: Number,
+    imageSrc: String
+  }
+}
+</script>
+
+<style lang="sass">
+.cover {
+  display: block;
+  color: white;
+  transition: all 200ms ease-in-out;
+
+  &:hover {
+    opacity: 0.6;
+    transition: all 100ms ease-in-out;
+  }
+
+  &:active {
+    transform: scale(0.97);
+    transition: all 100ms ease-in-out;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  p {
+    margin: 0;
+    font-weight: 500;
+
+    &.name {
+      margin-top: 8px;
+      text-transform: capitalize;
+      font-size: 16px;
+      line-height: 1.25em;
+    }
+
+    &.year {
+      font-size: 14px;
+      color: rgba(255, 255, 255, 0.5);
+    }
+  }
+
+  img {
+    display: block;
+    width: 100%;
+  }
+}
+
+@media screen and (max-width: 1263px) {
+  .cover {
+
+    p.name {
+      font-size: 15px;
+    }
+
+    p.year {
+      font-size: 14px;
+    }
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .cover {
+
+    p.name {
+      font-size: 14px;
+    }
+
+    p.year {
+      font-size: 13px;
+    }
+  }
+}
+</style>
---
+++ src/components/CoverList.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="cover-list">
+    <h3>{{ title }}</h3>
+    <ul class="better-scrollbar">
+      <li v-for="(cover, i) in covers" :key="i">
+        <cover
+          :type="cover.type"
+          :name="cover.name"
+          :year="cover.year"
+          :imageSrc="cover.imageSrc"
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Cover from '@/components/Cover.vue'
+
+export default {
+  name: "cover-list",
+  components: {
+    "cover": Cover
+  },
+  props: {
+    title: String,
+    covers: Array
+  }
+}
+</script>
+
+<style lang="sass">
+.cover-list {
+  width: 100%;
+  margin-top: 64px;
+
+  h3 {
+    margin-bottom: 16px;
+    font-size: 34px;
+    font-weight: 400;
+    line-height: 1.2em;
+    color: white;
+  }
+
+  ul {
+    display: flex;
+    overflow-x: auto;
+    padding: 8px 0;
+    list-style: none;
+  }
+
+  li {
+    flex-grow: 0;
+    flex-shrink: 0;
+    flex-basis: 20%;
+
+    &:not(:last-child) {
+      margin-right: 32px;
+    }
+  }
+}
+
+@media screen and (max-width: 1263px) {
+  .cover-list {
+    margin-top: 38px;
+  
+    h3 {
+      font-size: 28px;
+    }
+
+    li {
+      flex-basis: 28%;
+    }
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .cover-list {
+    margin-top: 32px;
+  
+    h3 {
+      font-size: 24px;
+    }
+
+    li {
+      flex-basis: 50%;
+    }
+  }
+}
+</style>
