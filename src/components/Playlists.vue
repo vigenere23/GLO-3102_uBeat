4f45bce0d0fdc23eba00f02144c25fcdb2f1057b
GASTP33 <gabriel.st-pierre.6@ulaval.ca>
2019-03-13 22:33:28 -0400

    added v-input + changed userId to ours

--- src/components/Playlists.vue
+++ src/components/Playlists.vue
@@ -4,10 +4,18 @@
 
       <div class="centered-title">
         <h1 class="large-thin-title">Playlists</h1>
-        <h2 class="large-thin-subtitle">for {{username}}</h2>
-        <input class="inputStyle" type="text" v-model="newPlayList" placeholder="new Playlist">
-        <v-btn v-on:click="addPlaylist "> Add playlist </v-btn>
+        <h2 class="large-thin-subtitle">for {{ user.name }}</h2>
+      </div>
 
+      <div class="add-playlist">
+        <v-text-field
+          label="playlist-name"
+          placeholder="Name..."
+          solo
+          hide-details
+          v-model="newPlayListName"
+        ></v-text-field>
+        <v-btn v-on:click="addPlaylist"> Add playlist </v-btn>
       </div>
 
       <cover-list
@@ -31,9 +39,9 @@ export default {
   },
   data() {
     return {
-      username: '',
+      user: {},
       playlists: [],
-      newPlayList: ''
+      newPlayListName: ''
     };
   },
   async mounted() {
@@ -54,30 +62,44 @@ export default {
       this.username = '';
     },
     async loadUserInfos(userId) {
-      const infos = await api.getUserInfos(userId);
-      this.username = infos.name;
+      const user = await api.getUserInfos(userId);
+      console.log(user);
+      this.user = user;
     },
     async loadPlaylists(userId) {
       const playlists = await api.getUserPlaylists(userId);
       this.playlists = playlists;
     },
     async addPlaylist() {
-      if (this.newPlayList === '') {
-        return false;
-      } else if (this.newPlayList !== '') {
+      if (this.newPlayListName !== '') {
         const infos = await api.getUserInfos(this.$route.params.userId);
-        await api.addPlaylist(this.newPlayList, infos.email);
-        this.newPlayList = '';
-        setTimeout(this.refreshPage, 100);
-        return true;
+        await api.addPlaylist(this.newPlayListName, infos.email);
+        this.playlists.push({
+          name: this.newPlayListName,
+          owner: this.user,
+          tracks: []
+        });
+        this.newPlayListName = '';
       }
-      return false;
-    },
-    async refreshPage() {
-      await this.loadPage(this.$route.params.userId);
     }
   }
 };
 </script>
 
+<style lang="scss">
+.add-playlist {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
 
+  button {
+    flex-shrink: 0;
+  }
+
+  .v-input {
+    max-width: 300px;
+    min-width: 100px
+  }
+}
+</style>
