7670fa5daf1fae386f2531fde8db6a4435e4a5d0
Nicolas <nicolas.fillion.4@ulaval.ca>
2019-03-14 19:04:25 -0400

    finalisation suppression chansons playlist

--- src/components/TrackListItem.vue
+++ src/components/TrackListItem.vue
@@ -1,30 +1,33 @@
 <template>
-  <v-hover>
-    <v-list-tile class="album-tracks-list-item" slot-scope="{ hover }">
-      <v-list-tile-avatar class="song-number">
-        <div>
-          <v-list-tile-title v-if="!hover">{{ number }}</v-list-tile-title>
-          <v-list-tile-title v-else class="play-arrow" v-on:click="play">
-            <v-btn icon ripple>
-              <v-icon color="white">play_arrow</v-icon>
-            </v-btn>
-          </v-list-tile-title>
-        </div>
-      </v-list-tile-avatar>
-      <v-list-tile-content>
-        <v-list-tile-title class="song-title">{{ title }}</v-list-tile-title>
-        <v-list-tile-sub-title class="song-duration">{{ durationText || '' }}</v-list-tile-sub-title>
-      </v-list-tile-content>
-      <v-list-tile-action v-on:click="deleteSong">
-        <v-btn icon ripple v-if="listType === 'playlist'">
-          <v-icon color="white">remove</v-icon>
-        </v-btn>
-        <v-btn icon ripple v-else>
-          <v-icon color="white">add</v-icon>
-        </v-btn>
-      </v-list-tile-action>
-    </v-list-tile>
-  </v-hover>
+  <div v-if="deleted === false">
+    <v-hover>
+      <v-list-tile class="album-tracks-list-item" slot-scope="{ hover }">
+        <v-list-tile-avatar class="song-number">
+          <div>
+            <v-list-tile-title v-if="!hover">{{ number }}</v-list-tile-title>
+            <v-list-tile-title v-else class="play-arrow" v-on:click="play">
+             <v-btn icon ripple>
+               <v-icon color="white">play_arrow</v-icon>
+             </v-btn>
+            </v-list-tile-title>
+          </div>
+        </v-list-tile-avatar>
+        <v-list-tile-content>
+         <v-list-tile-title class="song-title">{{ title }}</v-list-tile-title>
+         <v-list-tile-sub-title class="song-duration">{{ durationText || '' }}</v-list-tile-sub-title>
+        </v-list-tile-content>
+       <v-list-tile-action v-on:click="deleteSong">
+         <v-btn icon ripple v-if="listType === 'playlist'">
+            <v-icon color="white">remove</v-icon>
+          </v-btn>
+          <v-btn icon ripple v-else>
+            <v-icon color="white">add</v-icon>
+         </v-btn>
+       </v-list-tile-action>
+     </v-list-tile>
+   </v-hover>
+  </div>
+
 
 </template>
 
@@ -49,6 +52,11 @@ export default {
       return helper.getPrettyDuration(this.duration);
     }
   },
+  data() {
+    return {
+      deleted: false
+    };
+  },
   methods: {
     play() {
       MusicControl.stopSong();
@@ -56,6 +64,7 @@ export default {
     },
     async deleteSong() {
       await api.deleteSongTrackFromPlaylist(this.playlistID, this.trackKey);
+      this.deleted = true;
     }
   }
 
