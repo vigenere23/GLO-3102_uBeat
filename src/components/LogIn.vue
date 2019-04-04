9ba69087e7a510afaccaa39472ab744d9783cf3d
Olivier <olivier.gingras.1@ulaval.ca>
2019-04-04 11:55:42 -0400

    ui basic for login/signup

---
+++ src/components/LogIn.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+  <div class="simple-input-field">
+    <v-text-field
+      label="user-name"
+      placeholder="Name..."
+      solo
+      hide-details
+      v-model="userName">
+
+    </v-text-field>
+    </div>
+
+    <div class="simple-input-field">
+      <v-text-field
+        label="user-name"
+        placeholder="Password..."
+        type="password"
+        solo
+        hide-details
+        v-model="userPassword">
+
+      </v-text-field>
+    </div>
+
+    <div id="logIn">
+      <v-btn v-on:click="addPlaylist">Log In</v-btn>
+    </div>
+
+  </div>
+</template>
+
+<script>
+    export default {
+      name: 'Profile',
+      data() {
+        return {
+          userName: '',
+          userPassword: ''
+        };
+      },
+    };
+</script>
+
+<style scoped>
+  #logIn{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+</style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="simple-input-field">
+      <v-text-field
+        label="user-name"
+        placeholder="Name..."
+        solo
+        hide-details
+        v-model="userName">
+
+      </v-text-field>
+    </div>
+
+    <div class="simple-input-field">
+      <v-text-field
+        label="email"
+        placeholder="Email..."
+        solo
+        hide-details
+        v-model="userEmail">
+
+      </v-text-field>
+    </div>
+
+    <div class="simple-input-field">
+      <v-text-field
+        label="password"
+        placeholder="Password..."
+        type="password"
+        solo
+        hide-details
+        v-model="userPassword">
+
+      </v-text-field>
+    </div>
+
+    <div class="simple-input-field">
+      <v-text-field
+        label="confirm-password"
+        placeholder="Confirm Password..."
+        type="password"
+        solo
+        hide-details
+        v-model="userConfirmPassword">
+
+      </v-text-field>
+    </div>
+
+    <v-btn id="SignUp" v-on:click="addPlaylist">Sign Up</v-btn>
+
+  </div>
+    
+</template>
+
+<script>
+    export default {
+      name: 'LogIn',
+      data() {
+        return {
+          userName: '',
+          userPassword: '',
+          userConfirmPassword: '',
+          userEmail: '',
+        };
+      },
+    };
+</script>
+
+<style scoped>
+  #SignUp{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+</style>
