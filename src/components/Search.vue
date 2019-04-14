8d103e341e82af9d1a8cc29583f01e089de0e016
Maxime St-Pierre <maxime.st-pierre.5@ulaval.ca>
2019-04-14 18:59:23 -0400

    wip

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -2,12 +2,24 @@
   <div>
     <v-list
       dark>
+      <v-subheader inset>Artists</v-subheader>
       <v-list-tile
-        v-for="result in results"
-        three-line>
-        <v-list-tile-action v-if="result.wrapperType === 'collection'">
-          <v-img :src="result.artworkUrl60"></v-img>
-        </v-list-tile-action>
+        v-for="result in results.filter(function(item) {return item.wrapperType.match('artist')})">
+        <result :result="result" type="type"></result>
+      </v-list-tile>
+      <v-subheader inset>Albums</v-subheader>
+      <v-list-tile
+        v-for="result in results.filter(function(item) {return item.wrapperType.match('collection')})">
+        <result :result="result" type="type"></result>
+      </v-list-tile>
+      <v-subheader inset>Tracks</v-subheader>
+      <v-list-tile
+        v-for="result in results.filter(function(item) {return item.wrapperType.match('track')})">
+        <result :result="result" type="type"></result>
+      </v-list-tile>
+      <v-subheader inset>Users</v-subheader>
+      <v-list-tile
+        v-for="result in results.filter(function(item) {return item.wrapperType === undefined})">
         <result :result="result" type="type"></result>
       </v-list-tile>
     </v-list>
@@ -37,8 +49,12 @@
       };
     },
     async beforeRouteUpdate(to, from, next) {
+      this.cookie = Cookies.get('uBeatCookie');
       this.type = to.params.type;
-      this.results = await ubeat.search(to.params.type, to.params.query, this.cookie);
+      if (!(this.cookie === null || this.cookie === undefined || this.cookie === '')) {
+        this.type = this.$route.params.type;
+        this.results = await ubeat.search(to.params.type, to.params.query, this.cookie);
+      }
       next();
     }
   };
